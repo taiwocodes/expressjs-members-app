b4244cac661dbd4e8dd68c5ec60b1532adc85acb
Taiwo <famaktaiwo7@gmail.com>
2022-02-09 12:04:14 +0100

    first commit

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {   
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {   
+        id: 2,
+        name: 'Bob Williams',
+        email: 'bob@gmail.com',
+        status: 'active'
+    },
+    {  
+        id: 3,
+        name: 'Shannn Jack',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+    ];
+
+    module.exports = members;
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const exphbs =  require('express-handlebars');
+const logger = require('./middleware/logger');
+const members = require('./Members')
+
+const app = express();
+
+//init middleware
+// app.use(logger);
+
+// handlebars middleware
+// app.engine('handlebars', engine());
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//body parser middleware
+app.use(express.json()); //handle raw json
+app.use(express.urlencoded({extended: false})); //handle form submission
+
+//homepage
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member App',
+    members
+})
+);
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//members api routes
+app.use('/api/members', require('./routes/api/members'))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
